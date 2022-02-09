221aeda0a1f8622c8ba0e55426e75204e45d17c6
Sebastien Baeyens <sesebaey@outlook.be>
2022-02-09 23:51:24 +0100

    jeux a debug.py

--- justePrix.py
+++ justePrix.py
@@ -1,2 +1,15 @@
+from random import randint
 print("bonjour et bienvenue aux juste prix")
-reponse = input("entré un prix : ")
+
+reponse = int(input("entré un prix : "))
+prix = ""
+while prix != reponse:
+    prix = randint(1, 100)
+    if prix == reponse:
+        print("bravo bien joué")
+    elif prix > reponse:
+        print(f"plus grande que {reponse}")
+        reponse = int(input("entré un prix : "))
+    elif prix < reponse:
+        print(f"plus petit que {reponse}")
+        reponse = int(input("entré un prix : "))
