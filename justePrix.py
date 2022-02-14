8e7f8446fde6bba69af6bad8c03b9923138a6432
Sebastien Baeyens <sesebaey@outlook.be>
2022-02-14 13:33:13 +0100

    jeuxFini.py

--- justePrix.py
+++ justePrix.py
@@ -1,15 +1,16 @@
 from random import randint
 print("bonjour et bienvenue au juste prix")
-
+prix = randint(1, 100)
 reponse = int(input("entrer un prix : "))
-prix = ""
+
+
 while prix != reponse:
-    prix = randint(1, 100)
-    if prix == reponse:
-        print("bravo bien joué")
-    elif prix > reponse:
+
+    if prix > reponse:
         print(f"plus grande que {reponse}")
         reponse = int(input("entrer un prix: "))
-    elif prix < reponse:
+    else:
         print(f"plus petit que {reponse}")
         reponse = int(input("entrer un prix: "))
+if prix == reponse:
+    print("bravo bien joué")
